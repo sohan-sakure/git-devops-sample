4efd461c21e2d2a0d7d1cfb7e07f147e7886118a
sohan <rock2sohan@gmail.com>
2020-01-06 14:40:57 +0530

    Added proper c# code

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,1 +1,17 @@
-"Hello World" 
+using System;
+using System.IO;
+
+namespace Test.DevOps
+{
+
+    public class Program
+    {
+        public static void main(string[] args)
+        {
+            Console.WriteLine("Hello World);            
+        }
+    }
+    
+}
+
+
